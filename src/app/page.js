121aaee8be8509266357adb4bebbbf1fc428eed9
noelbluu <panida.stp@icloud.com>
2023-07-28 22:35:43 +0700

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,14 @@ export default function RegisFormPage() {
   const [fname, setFname] = useState("");
   const [fnameError, setFnameError] = useState(false);
   const [lname, setLname] = useState("");
+  const [lnameError, setLnameError] = useState(false);
   const [plan, setPlan] = useState("");
+  const [planError, setPlanError] = useState(false);
   const [gender, setGender] = useState(null);
   const [buyBottle, setBuyBottle] = useState(false);
   const [buyShoes, setBuyShoes] = useState(false);
   const [buyCap, setBuyCap] = useState(false);
+  const [agree, setAgree] = useState(false);
 
   const inputFnameOnChange = (event) => {
     setFnameError(false);
@@ -17,10 +20,12 @@ export default function RegisFormPage() {
   };
 
   const inputLnameOnChange = (event) => {
+    setLnameError(false);
     setLname(event.target.value);
   };
 
   const selectPlanOnChange = (event) => {
+    setPlanError(false);
     setPlan(event.target.value);
   };
 
@@ -44,8 +49,13 @@ export default function RegisFormPage() {
     setBuyCap(event.target.checked);
   };
 
+  const cbCheckAgree = (event) => {
+    setAgree(event.target.checked);
+  };
+
   function computeTotalPayment() {
     let total = 0;
+    
     if (plan === "funrun") total += 500;
     if (plan === "mini") total += 800;
     if (plan === "half") total += 1200;
@@ -59,14 +69,31 @@ export default function RegisFormPage() {
 
   const registerBtnOnClick = () => {
     let fnameOk = true;
+    let lnameOk = true;
+    let planOk = true;
+
     if (fname === "") {
       fnameOk = false;
       setFnameError(true);
     }
 
+    if (lname === "") {
+      lnameOk = false;
+      setLnameError(true);
+    }
+
+    if (plan == "") {
+      planOk = false;
+      setPlanError(true);
+    }
+
+    if (gender == null) {
+      setGender(false);
+    }
+
     if (fnameOk) {
       alert(
-        `Registration complete. Please pay money for ${computeTotalPayment().toLocaleString()} THB.`
+        `Registration complete. Please pay money for ${buyBottle && buyCap && buyShoes ? (computeTotalPayment() * 80) / 100 : computeTotalPayment()} THB.`
       );
     }
   };
@@ -88,7 +115,7 @@ export default function RegisFormPage() {
         <div>
           <label className="form-label">Last name</label>
           <input
-            className="form-control"
+            className={"form-control" + (lnameError ? " is-invalid" : "")}
             onChange={inputLnameOnChange}
             value={lname}
           />
@@ -100,7 +127,7 @@ export default function RegisFormPage() {
       <div>
         <label className="form-label">Plan</label>
         <select
-          className="form-select"
+          className={"form-select" + (planError ? " is-invalid" : "")}
           onChange={selectPlanOnChange}
           value={plan}
         >
@@ -131,9 +158,11 @@ export default function RegisFormPage() {
             checked={gender === "female"}
           />
           Female 👩
-          {/* To show error when user did not select gender, */}
-          {/* We just have to render the div below (Not using is-invalid bootstrap class) */}
-          {/* <div className="text-danger">Please select gender</div> */}
+          {gender == false ? (
+            <div className="text-danger">Please select gender</div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
 
@@ -175,23 +204,36 @@ export default function RegisFormPage() {
 
       {/* Total Payment */}
       <div>
-        Total Payment : {computeTotalPayment().toLocaleString()} THB
-        {/* Render below element conditionally when user get 20% discount */}
-        {/* <span className="text-success d-block">(20% Discounted)</span> */}
+        Total Payment :{" "}
+        {buyBottle && buyCap && buyShoes
+          ? (computeTotalPayment() * 80) / 100
+          : computeTotalPayment()}{" "}
+        THB
+
+        {buyBottle && buyCap && buyShoes ? (
+          <span className="text-success d-block">(20% Discounted)</span>
+        ) : (
+          ""
+        )}
       </div>
 
       {/* Terms and conditions */}
       <div>
-        <input className="me-2" type="checkbox" />I agree to the terms and
-        conditions
+        <input
+          className="me-2"
+          type="checkbox"
+          onChange={cbCheckAgree}
+          checked={agree}
+        />
+        I agree to the terms and conditions
       </div>
 
       {/* Register Button */}
       <button
         className="btn btn-success my-2"
         onClick={registerBtnOnClick}
-        //You can embbed a state like below to disabled the button
-        //disabled={isUserAgreed}
+
+        disabled={!agree}
       >
         Register
       </button>
